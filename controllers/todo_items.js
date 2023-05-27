74e7965e6c6202d82bb2af4c5b642664adc16215
palani <palanibaba2002@gmail.com>
2023-05-27 11:59:10 +0530

    ADD. Included Patch, Update, and Delete

--- controllers/todo_items.js
+++ controllers/todo_items.js
@@ -2,8 +2,16 @@ const getAllToDoItems = (req,res)=>{
     res.send("List of all to-do List");
 }
 
-const createANewToDoItems = (req,res)=>{
-   
+ const createANewToDoItems = (req,res)=>{
+    res.json(req.body)
+ }
+ const getAToDoItem =(req,res)=>{
+    res.send(`The Id is ${req.params.id}`)
+ }
+const updateAToDoItem = (req,res)=>{
+    res.send(`Updating item with id ${req.params.id}`)
 }
-
-module.exports = {getAllToDoItems,createANewToDoItems}
+const deleteAToDoItem = (req,res)=>{
+    res.send(`Deleting item with id${req.params.id}`)
+}
+module.exports = {getAllToDoItems,createANewToDoItems,updateAToDoItem,deleteAToDoItem,getAToDoItem}
--- routes/todo_items.js
+++ routes/todo_items.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const router = express.Router();
-const {getAllToDoItems,createANewToDoItems} = require('../controllers/todo_items')
+
+const {getAllToDoItems,createANewToDoItems,getAToDoItem,updateAToDoItem,deleteAToDoItem} = require('../controllers/todo_items')
 router.route('/').get(getAllToDoItems).post(createANewToDoItems)
+router.route('/:id').get(getAllToDoItems)
+router.route('/id:').get(getAllToDoItems).update(updateAToDoItem)
+router.route('/id:').patch(updateAToDoItem).delete(deleteAToDoItem)
 module.exports = router
